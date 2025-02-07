a11fcabbdafa3a8ea293f3c4f5e91757118696ee
silvialucas <silvialucas@celepar.pr.gov.br>
2025-02-07 15:41:29 -0300

    Configuração Flyway e mysql

--- src/test/java/br/com/sicredi/canaisdigitais/avaliacaotecnicacanais/AvaliacaoTecnicaCanaisApplicationTests.java
+++ src/test/java/br/com/sicredi/canaisdigitais/avaliacaotecnicacanais/AvaliacaoTecnicaCanaisApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.sicredi.canaisdigitais.avaliacaotecnicacanais;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AvaliacaoTecnicaCanaisApplicationTests {
 
     @Test
